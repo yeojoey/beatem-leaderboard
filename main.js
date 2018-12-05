53b7d065ee63bd4c1e92c68398f6217aa27e8dd6
Yuan Yuchuan <yyc478@gmail.com>
2018-12-06 01:30:21 +0800

    Revert "Add secret check"

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ const bodyParser = require('body-parser')
 const sass = require('node-sass-middleware')
 const PORT = process.env.PORT || 3000
 const MONGODB_PATH = process.env.MONGODB_PATH || 'mongodb://localhost:27017/leaderboard'
-const password = process.env.LEADERBOARD_SECRET
-
-if (password !== undefined) {
-  console.log(`Using password ${password}`);
-}
 
 const db = require('./app/databaseHandler')(MONGODB_PATH)
 
@@ -25,60 +20,46 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function (request, response) {
   db.getAllEntries().then((documents) => {
-      // Sort entries by score, highest to lowest
-      documents.sort((a, b) => {
-        return b.score - a.score
-      })
-      response.render('index', {
-        entries: documents
-      }) //render sorted scores to the index page
+    // Sort entries by score, highest to lowest
+    documents.sort((a, b) => {
+      return b.score - a.score
     })
-    .catch(() => {
-      response.status(500)
-      response.send()
-    })
+    response.render('index', {entries: documents}) //render sorted scores to the index page
+  })
+  .catch(() => {
+    response.status(500)
+    response.send()
+  })
 })
 
 app.get('/scores', (request, response) => {
   db.getAllEntries().then((documents) => {
-      documents.sort((a, b) => {
-        return b.score - a.score
-      })
-
-      for (var i = 0; i < documents.length; i++) {
-        delete documents[i]['_id'];
-        delete documents[i]['Timestamp'];
-      }
-
-      response.json(documents)
-      response.send()
+    documents.sort((a, b) => {
+      return b.score - a.score
     })
-    .catch(() => {
-      response.status(500)
-      response.send()
-    })
+
+    for(var i = 0; i < documents.length; i++) {
+      delete documents[i]['_id'];
+      delete documents[i]['Timestamp'];
+  }
+
+    response.json(documents)
+    response.send()
+  })
+  .catch(() => {
+    response.status(500)
+    response.send()
+  })
 })
 
 app.post('/scores', function (request, response) {
   console.log(request.body)
   var body = request.body
-
   if (!body.score && body.score !== 0) {
     response.status(400)
     response.send()
     return
   }
-
-  if (password !== undefined) {
-    // Password is set, check for password
-    if (body.secret === undefined || body.secret !== password) {
-      response.status(401)
-      response.send()
-      return
-    }
-  }
-  delete body.secret
-
   // Don't allow id's in post
   if (body.hasOwnProperty('_id')) {
     delete body._id
@@ -146,7 +127,7 @@ app.post('/shialabeouf', function (request, response) {
 
 
 app.use(sass({
-  /* Options */
+    /* Options */
   src: path.join(__dirname, 'sass'),
   dest: path.join(__dirname, 'public'),
   debug: true,
